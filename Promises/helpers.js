2a98a43afda237f85e14b6619cd7228052e55d27
Aman Velpula <velpmail@gmail.com>
2023-01-02 14:16:47 -0500

    promises debugging

---
+++ Promises/helpers.js
@@ -0,0 +1,7 @@
+export const wait = milliseconds => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, milliseconds);
+    });
+}
---
+++ Promises/promises.js
@@ -0,0 +1,10 @@
+import {wait} from "./helpers.js";
+
+const init = () => {
+    console.log("Program starterd");
+    wait(1000).then(() => {
+        console.log("Waiting 1 Second");
+    })
+}
+
+init();
