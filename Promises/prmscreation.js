6defedc05682c6d95c1a4559ea07e445b3ad79fa
Aman Velpula <velpmail@gmail.com>
2023-01-06 15:40:10 -0500

    promise creation cmt

--- Promises/prmscreation.js
+++ Promises/prmscreation.js
@@ -1,14 +1,19 @@
 
 
-//lets see how we can implement the waitOneSecond funciton tht
-//resolves successfully after 1 second. We start by defining the function:
+/* fix the implementation so that it returns to the fulfilled state after 1 second*/
 
-const waitOneSecond = () => { //defining the function
-    return new Promise (() => {// new function must return a promise
-        //this means the waitOneSeocnd function is doing some asynchronous
-        /* work so its result will not be directly accessibly because it 
-        resolves sometime in the future */
-        
 
-    });
+const wait = (milliseconds) => {
+    return new Promise(resolve => {
+
+        setTimeout(() =>{
+            resolve()
+        },milliseconds)
+
+    })
+
 }
+
+wait(1000).then(() => {
+    console.log("Done waiting!")
+});
