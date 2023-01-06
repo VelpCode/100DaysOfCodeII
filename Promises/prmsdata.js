0378d6a641050d55b0562bf616f1e55e8d204ec6
Aman Velpula <velpmail@gmail.com>
2023-01-06 15:40:04 -0500

    implementation of data without object

---
+++ Promises/prmsdata.js
@@ -0,0 +1,25 @@
+/*Implement fake fetch complete the fakeFetch
+function such that it returns a promise that resolves
+successfully after 1 second with the following object:
+{
+    departed: false,
+    delayed: true
+}
+*/
+
+const fakeFetch = () => {
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const fitch = {
+                departed 
+            }
+        })
+    })
+
+
+}
+
+fakeFetch().then(() => {
+    console.log(data);
+})
