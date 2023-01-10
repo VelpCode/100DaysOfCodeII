7a30ab5a6814dcf657cdd68b81649c885359a05b
Aman Velpula <velpmail@gmail.com>
2023-01-10 15:04:48 -0500

    visualizing the code with cons log

--- JSON/fetchFilter.js
+++ JSON/fetchFilter.js
@@ -1,22 +1,23 @@
-/*Let's say we're coding the Chapters sidebar of this website.
-You can get the list of chapters by using the following endpoint https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/chapters/all.json
-Call the displayCompletedChapters with only the chapters (array) that have been completed (it expects an array).
+/*
+Complete the function checkForNewNotifications such that it fetches the following endpoint https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json and then calls the imported showNewNotifications function with the number of unread notifications.
+Then, handle network errors by logging it to the console with console.error().
 */
 
-const getChapters = () => {
+const checkForNewNotifications = () => {
 
-    fetch(`https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/chapters/all.json`)
+    fetch(`https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json`)
     .then(result => result.json())
     .then(data => {
         console.log(data)
-        const completedChapters = data.filter(chapter => chapter.isCompleted)
-        displayCompletedChapters(completedChapters)
     })
 
+
+
 }
 
-function displayCompletedChapters(chapters) {
-    console.log("Recieved", chapters)
+function showNewNotifications(count) {
+    console.log(`You have ${count} new notifications, would you like to read them?`)
+
 }
 
-getChapters();
+checkForNewNotifications()
