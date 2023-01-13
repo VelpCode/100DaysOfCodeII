40e463abcb608e7ed6e9fe648b0dffe81f297ed4
Aman Velpula <velpmail@gmail.com>
2023-01-13 10:09:40 -0500

    made changes to index layout

---
+++ ecmascript/index.js
@@ -0,0 +1,17 @@
+
+import NumericHelper from "numeric-helpers";
+
+class isEvenNumber = number => {
+
+    if(number % 2 === 0) {
+        return true 
+    } else {
+        return false;
+    }
+
+}
+
+
+console.log(isEvenNumber(2));
+console.log(isEvenNumber(3));
+
