56130fe1b2636248554752bd12431dfb6433f4d7
Aman Velpula <velpmail@gmail.com>
2023-01-13 11:42:12 -0500

    fetchwrapper add

--- ecmascript/index.js
+++ ecmascript/index.js
@@ -1,17 +1,20 @@
 
-import NumericHelper from "numeric-helpers";
-
-class isEvenNumber = number => {
-
-    if(number % 2 === 0) {
-        return true 
-    } else {
-        return false;
+class FetchWrapper {
+    constructor(baseURL) {
+        this.baseURL = baseURL;
     }
 
+    get(endpoint) {
+        return fetch(this.baseURL + endpoint)
+        .then(response => response.json())
+    }
 }
 
 
-console.log(isEvenNumber(2));
-console.log(isEvenNumber(3));
+try {
+    const API = new FetchWrapper("https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app")
 
+    API.get("/notifications/new.json").then(data => {
+        console.log(data);
+    })
+} catch(error) {}
