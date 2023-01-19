00b92097db0e9e44214aae0a8cbdd2f02458a41e
Aman Velpula <velpmail@gmail.com>
2023-01-19 10:20:54 -0500

    practicing ANKi problems

--- sidemissions/fetch-wrapper.js
+++ sidemissions/fetch-wrapper.js
@@ -1,11 +1,12 @@
 export default class FetchWrapper {
 
     constructor(baseURL) {
-        this.baseURL = baseURL;
+        this.baseURL = baseURl;
     }
-
     get(endpoint) {
         return fetch(this.baseURL + endpoint)
             .then(response => response.json())
     }
+
+    
 }
