ed91b75c571811d6d92fac48d68f0a8766079183
Aman Velpula <velpmail@gmail.com>
2023-01-19 10:21:05 -0500

    using ANKi problemset

---
+++ ANKI/ankipractice.js
@@ -0,0 +1,11 @@
+
+class futureEleme = () => {
+
+    document.querySelectorAll("div")
+    document.querySelectorAll(".a")
+
+    return document.querySelector("#user-name").value = " "
+
+    return document.querySelector("links").textContent = "N/A"
+
+}
