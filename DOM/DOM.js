1944ea5d0f9241e2db2d24b85179de4a19a37589
Aman Velpula <velpmail@gmail.com>
2023-01-20 10:53:27 -0500

    fixing DOM outline

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -6,3 +6,19 @@ banner.getAttribute("id");
 banner.getAttribute("disabled");
 
 const button = document.querySelector("#login")
+button.getAttribute("disabled")
+
+
+banner.getAttribute(key)
+banner.setAttribute(key)
+banner.hasAttribute(key)
+
+return button.hasAttribute("disabled")
+
+//complete the enable button function such that it enables the button it recievies 
+/* all button id's are set to disabled make it so it's enabled
+*/
+
+button.removeAttribute("disabled")
+
+//ths attribute method will remove disabled from all id's so it can be enabled.
