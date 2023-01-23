5df7bad4c83ed9f8bc82f568a256ab2f7b0b98a3
Aman Velpula <velpmail@gmail.com>
2023-01-22 19:44:42 -0500

    updated styles.css

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -15,10 +15,6 @@ banner.hasAttribute(key)
 
 return button.hasAttribute("disabled")
 
-//complete the enable button function such that it enables the button it recievies 
-/* all button id's are set to disabled make it so it's enabled
-*/
-
 button.removeAttribute("disabled")
 
 //ths attribute method will remove disabled from all id's so it can be enabled.
