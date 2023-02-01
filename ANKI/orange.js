3c07d83c25e7d30918670433592c1404d5acb126
Aman Velpula <velpmail@gmail.com>
2023-01-31 20:39:16 -0500

    fixing up orange js

--- ANKI/orange.js
+++ ANKI/orange.js
@@ -5,6 +5,13 @@ const renderShoppingList = items => {
     })
 }
 
+button.addEventListener('click', ()=>{
+  msg.classList.toggle('reveal');
+})
+
+button.addEventListener('click', () => {
+  msg.classList.toggle('reveal')
+})
 
 const sampleList = ["Orange", "Banana", "Coffee", "Paper"];
 console.log(renderShoppingList(sampleList))
