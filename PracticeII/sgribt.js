6140144e8e278ce1820ac4a149dbcd68780c021f
Aman Velpula <velpmail@gmail.com>
2023-02-03 19:37:37 -0500

    layout changing

--- PracticeII/sgribt.js
+++ PracticeII/sgribt.js
@@ -4,7 +4,6 @@ const disableFunc = () => {
 
     const disable = document.querySelector("#btn-disable")
     const enable = document.querySelector("#btn-enable")
-    const start = document.querySelector("#btn-start")
 
     disable.addEventListener("click", () => {
         start.setAttribute("disabled", "disabled")
