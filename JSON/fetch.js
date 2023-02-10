e5c0747ebfe79111ca9b68b43980a0a24851c7a9
Aman Velpula <velpmail@gmail.com>
2023-02-09 23:09:52 -0500

    Huberman

--- JSON/fetch.js
+++ JSON/fetch.js
@@ -1,11 +1,11 @@
 /*Complete the checkForNewNotifications function such that it makes a fetch request to https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json and return its result.
 Also, visualize that the result of fetch is a Promise. You should see Promise in the console.*/
 
-const checkForNewNotifications = () => {
+const checkHubermanNotis = () => {
 
     return fetch('https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app/notifications/new.json')
 
 }
 
-const result = checkForNewNotifications();
+const result = checkHubermanNotis();
 console.log(result)
