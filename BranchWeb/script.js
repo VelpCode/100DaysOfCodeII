e2a99b83b49b338f51f399757a005585ec47754a
Aman Velpula <velpmail@gmail.com>
2023-02-17 18:33:00 -0500

    created script file

--- BranchWeb/script.js
+++ BranchWeb/script.js
@@ -14,22 +14,26 @@ banner.getAttribute("id");
 banner.getAttribute("disabled");
 
 const button = document.querySelector("#login");
-button.getAttribute("disbaled")
-button.removeAttribute("disabled")
-button.setAttribute("id", "navbar")
+button.getAttribute("disbaled");
+button.removeAttribute("disabled");
+button.setAttribute("id", "navbar");
 
 const positions = document.querySelector("#job-positions");
-positions.insertAdjacentHTML("afterbegin", `<div class = "position">2015-2020</div>`)
-positions.insertAdjacentHTML("beforeend", `<div class = "positiontwo">Amma Nana</div>`)
+positions.insertAdjacentHTML("afterbegin", `<div class = "position">2015-2020</div>`);
+positions.insertAdjacentHTML("beforeend", `<div class = "positiontwo">Amma Nana</div>`);
 
 const discontinue = document.querySelector("#job-positions");
-positions.insertAdjacentHTML("beforeend", `div class = "document">Documentation</div>`)
-positions.insertAdjacentHTML("afterbegin", 'div class = "document">Documentation 2 </div>')
+positions.insertAdjacentHTML("beforeend", `div class = "document">Documentation</div>`);
+positions.insertAdjacentHTML("afterbegin", 'div class = "document">Documentation 2 </div>');
 
 
 const docuSelector = document.querySelector("main");
 docuSelector.insertAdjacentHTML("beforeend", `div class = "selector">DocumentationScript</div>`)
 docuSelector.insertAdjacentHTML("afterbegin", `div class = "strong">Strong selector</div>`)
+
+const createBanner = document.querySelector("#main");
+createBanner = insertAdjacentHTML(`beforeend`, `<div class = "selector">Documentation three</div>`)
+createBanner = insertAdjacentHTML(`afterbegin`, `<div class = "selector">Formatting error</div>`);
 
 
 
