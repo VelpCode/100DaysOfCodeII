8f04084cdeffc8b01740ff1be32d8d54d01c7f7f
Aman Velpula <velpmail@gmail.com>
2023-02-19 23:10:28 -0500

    removing clutter

--- BranchWeb/script.js
+++ BranchWeb/script.js
@@ -22,25 +22,9 @@ const positions = document.querySelector("#job-positions");
 positions.insertAdjacentHTML("afterbegin", `<div class = "position">2015-2020</div>`);
 positions.insertAdjacentHTML("beforeend", `<div class = "positiontwo">Amma Nana</div>`);
 
-const discontinue = document.querySelector("#job-positions");
-positions.insertAdjacentHTML("beforeend", `div class = "document">Documentation</div>`);
-positions.insertAdjacentHTML("afterbegin", 'div class = "document">Documentation 2 </div>');
-
-
-const docuSelector = document.querySelector("main");
-docuSelector.insertAdjacentHTML("beforeend", `div class = "selector">DocumentationScript</div>`)
-docuSelector.insertAdjacentHTML("afterbegin", `div class = "strong">Strong selector</div>`)
-
-const createBanner = document.querySelector("#main");
-createBanner = insertAdjacentHTML(`beforeend`, `<div class = "selector">Documentation three</div>`)
-createBanner = insertAdjacentHTML(`afterbegin`, `<div class = "selector">Formatting error</div>`);
-
 const davidbut = document.querySelector("#david")
 davidbut.addEventListener("click", event => {
     console.log(davidbut.textContent)
 }, {
     once: true
 })
-
-setWelcomeMessage("Daarla")
-setWelcomeMessage("phrabu")
