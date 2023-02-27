bd0f958fd5d2ad71771574a927bea80f77bcc16f
Aman Velpula <velpmail@gmail.com>
2023-02-26 20:20:16 -0500

    Index

---
+++ Mamooty/index.js
@@ -0,0 +1,6 @@
+// TODO: Update output when the user selects a new language
+
+import {getTranslation} from "./helpers.js"
+
+const dropdown = document.querySelector("#languages-list");
+const output = document.querySelector("#output-text")
