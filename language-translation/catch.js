74da736a46d54232e8770c0e26ee3497da01be03
Aman Velpula <velpmail@gmail.com>
2023-03-01 12:52:46 -0500

    making func

--- language-translation/catch.js
+++ language-translation/catch.js
@@ -2,11 +2,12 @@
 /*Visualize async function, Run the code and see how the getRandomNumber 
 function returns a random number between 0 and 9 */
 
-const getRandomNumber = async () => {
-    const randomNumber = Math.floor(Math.random() * 10);
-    return randomNumber;
+const getMinimumVotingAge = async () => {
+    return 18;
 }
 
-getRandomNumber().then(value => {
-    console.log(value);
+//Finish Sample usage
+
+getMinimumVotingAge().then(value => {
+    console.log(value)
 })
