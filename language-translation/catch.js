8156006a0deb52ab8deae386cf3f4df8e74f8215
Aman Velpula <velpmail@gmail.com>
2023-03-01 12:19:42 -0500

    await/async

--- language-translation/catch.js
+++ language-translation/catch.js
@@ -1,10 +1,9 @@
-const getUsers = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        });
-}
 
-// Sample usage
-getUsers();
+
+const getUsers = async () => {
+
+    const response = await;
+    fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json
+
+}
