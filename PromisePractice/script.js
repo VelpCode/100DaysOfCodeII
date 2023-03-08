256d5000ecc30c7e44a0ba5e345e44f3c081cc50
Aman Velpula <velpmail@gmail.com>
2023-03-07 23:09:09 -0500

    making async method

--- PromisePractice/script.js
+++ PromisePractice/script.js
@@ -3,7 +3,20 @@
 const failedPromise = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            reject
-        })
+            reject("Something went wrong")
+        }, 500);
     })
 }
+
+
+const init = async () => {
+    try {
+        const result = await failedPromise();
+        console.log("You will never see me");
+    } catch(error) {
+        console.log("There was an error")
+        console.log(error);
+    }
+}
+
+init();
